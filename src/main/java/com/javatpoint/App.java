1655cb40e3b1849231707de88f834159b3c69547
Ajay Kumar <ajay011.sharma@hotmail.com>
2018-10-05 16:59:48 +0000

    Second major commit

--- src/main/java/com/javatpoint/App.java
+++ src/main/java/com/javatpoint/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "First change from Git Side" );
+        System.out.println( "First change from Git Side...Wow" );
     }
 }
